98fa7fd6bce1c6c7debac8537f96acd8de17c94c
ilhamsj <ilhamsaputrajati@gmail.com>
2020-05-20 11:59:21 +0700

    new ui

--- js/url.js
+++ js/url.js
@@ -1,13 +1,12 @@
-// const host = "http://localhost:8012/api/v1";
-const host =
-  "https://cors-anywhere.herokuapp.com/http://elogistic.ms.demo-qs.xyz:8010/api/v1";
+const host = "http://localhost:8012/api/v1";
+// const host = "https://cors-anywhere.herokuapp.com/http://elogistic.ms.demo-qs.xyz:8010/api/v1";
 
 var MAIN_GetTransactionsType_Url = `${host}/transaction`;
 var MAIN_GetDocCodeCustoms_url = `${host}/document`;
 var MAIN_GetCoreor_url = `${host}/coreor`;
 var MAIN_GetDocumentCustomsNGen_url = `${host}/document/custom`;
 var BILLING_ConfirmTransaction_url = `${host}/transaction/confirm`;
-var checkStatusContainer_url = `${host}/container`; // single/show
+var checkStatusContainer_url = `${host}/container`;
 var storeStatusContainer_url = `${host}/container`;
 var BILLING_GetBillingDetail_url = `${host}/billing/detail`;
 var BILLING_GetInvoice_url = `${host}/billing/invoice`;
